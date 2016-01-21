f138f5667fc2823b2f1ae24f6b7e32961894c787
Joel Franusic <joel.franusic@okta.com>
2016-01-21 15:19:53 -0800

    Update to fix issue when running on Heroku

--- app.py
+++ app.py
@@ -183,4 +183,4 @@ if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     if port == 5000:
         app.debug = True
-    app.run(port=port)
+    app.run("0.0.0.0", port=port)
